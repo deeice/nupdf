0f6293cdf0f7e434b9b82907c2cbe70faa965f05
deeice <dheyse@gmail.com>
2012-07-16 00:14:23 -0400

    Updated to use more recent mupdf 0.9, and added Zipit kbd support.

--- pdfapp.c
+++ pdfapp.c
@@ -20,18 +20,35 @@ static void pdfapp_error(pdfapp_t *app, fz_error error)
 
 void pdfapp_init(pdfapp_t *app)
 {
+#if 0
 	fz_error error;
 	memset(app, 0, sizeof(pdfapp_t));
 	error = fz_newrenderer(&app->rast, pdf_devicergb, 0, 1024 * 512);
 	if (error)
 		pdfapp_error(app, error);
+#else
+	memset(app, 0, sizeof(pdfapp_t));
+#endif
 }
 
 void pdfapp_open(pdfapp_t *app, char *filename)
 {
+#if 0
 	fz_error error;
 	fz_obj *obj;
 	char *password = "";
+#else
+	fz_error error;
+	fz_stream *file;
+	char *password = "";
+	fz_obj *obj;
+	fz_obj *info;
+
+	int fd;
+	fd = open(filename, O_BINARY | O_RDONLY, 0666);
+	if (fd < 0)
+		fprintf(stderr, "error, file %s does not exist\n", filename);
+#endif
 
 	/*
 	 * Open PDF and load xref table
@@ -39,11 +56,12 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 
 	app->filename = filename;
 
+#if 0
 	app->xref = pdf_newxref();
 	error = pdf_loadxref(app->xref, filename);
 	if (error)
 	{
-		fz_catch(error, "trying to repair");
+		fz_catch(error, "trying to air");
 		error = pdf_repairxref(app->xref, filename);
 		if (error)
 			pdfapp_error(app, error);
@@ -52,19 +70,27 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 	error = pdf_decryptxref(app->xref);
 	if (error)
 		pdfapp_error(app, error);
+#else
+	file = fz_open_fd(fd);
+	error = pdf_open_xref_with_stream(&app->xref, file, NULL);
+	if (error)
+		pdfapp_error(app, fz_rethrow(error, "cannot open document '%s'", filename));
+	fz_close(file);
+#endif
 
 	/*
 	 * Handle encrypted PDF files
 	 */
 
-	if (pdf_needspassword(app->xref))
+	if (pdf_needs_password(app->xref))
 	{
-		int okay = pdf_authenticatepassword(app->xref, password);
+		int okay = pdf_authenticate_password(app->xref, password);
 		while (!okay)
 		{	
+		        //password = winpassword(app, filename);
 			if (!password)
 				exit(1);
-			okay = pdf_authenticatepassword(app->xref, password);
+			okay = pdf_authenticate_password(app->xref, password);
 			if (!okay)
 				pdfapp_warn(app, "Invalid password.");
 		}
@@ -75,6 +101,7 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 	 * TODO: move this into mupdf library
 	 */
 
+#if 0
 	obj = fz_dictgets(app->xref->trailer, "Root");
 	app->xref->root = fz_resolveindirect(obj);
 	if (!app->xref->root)
@@ -103,13 +130,36 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 			app->doctitle = pdf_toutf8(obj);
 		}
 	}
+#else
+	app->outline = pdf_load_outline(app->xref);
+
+	app->doctitle = filename;
+	if (strrchr(app->doctitle, '\\'))
+		app->doctitle = strrchr(app->doctitle, '\\') + 1;
+	if (strrchr(app->doctitle, '/'))
+		app->doctitle = strrchr(app->doctitle, '/') + 1;
+	info = fz_dict_gets(app->xref->trailer, "Info");
+	if (info)
+	{
+		obj = fz_dict_gets(info, "Title");
+		if (obj)
+			app->doctitle = pdf_to_utf8(obj);
+	}
+#endif
 
 	/*
 	 * Start at first page
 	 */
 
+#if 0
 	app->pagecount = pdf_getpagecount(app->xref);
+#else
+	error = pdf_load_page_tree(app->xref);
+	if (error)
+		pdfapp_error(app, fz_rethrow(error, "cannot load page tree"));
 
+	app->pagecount = pdf_count_pages(app->xref);
+#endif
 	
 	app->rotate = 0;
 	
@@ -117,6 +167,7 @@ void pdfapp_open(pdfapp_t *app, char *filename)
 
 void pdfapp_close(pdfapp_t *app)
 {
+#if 0
 	if (app->page)
 		pdf_droppage(app->page);
 	app->page = nil;
@@ -135,6 +186,32 @@ void pdfapp_close(pdfapp_t *app)
 
 	pdf_closexref(app->xref);
 	app->xref = nil;
+#else
+	if (app->cache)
+		fz_free_glyph_cache(app->cache);
+	app->cache = NULL;
+
+	if (app->image)
+		fz_drop_pixmap(app->image);
+	app->image = NULL;
+
+	if (app->outline)
+		pdf_free_outline(app->outline);
+	app->outline = NULL;
+
+	if (app->xref)
+	{
+		if (app->xref->store)
+			pdf_free_store(app->xref->store);
+		app->xref->store = NULL;
+
+		pdf_free_xref(app->xref);
+		app->xref = NULL;
+	}
+
+	fz_flush_warnings();
+#endif
+
 }
 
 
--- pdfapp.h
+++ pdfapp.h
@@ -12,19 +12,36 @@ struct pdfapp_s
 	char *filename;
 	char *doctitle;
 	pdf_xref *xref;
-	
+#if 0	
 	/*pdf_outline *outline;*/
 	fz_renderer *rast;
+
 	int pagecount;
+#else
+	pdf_outline *outline;
+
+	int pagecount;
+	fz_glyph_cache *cache;
+#endif
 
 	/* current view params */
 	float zoom;
 	int rotate;
 	fz_pixmap *image;
 
+#if 0
 	/* current page params */
 	int pageno;
 	pdf_page *page;
+#else
+	/* current page params */
+	int pageno;
+	fz_rect page_bbox;
+	float page_rotate;
+	fz_display_list *page_list;
+	fz_text_span *page_text;
+	pdf_link *page_links;
+#endif
 
 	/* window system sizes */
 	int scrw, scrh;
